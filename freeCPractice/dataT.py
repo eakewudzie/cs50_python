e2bf54b8b4e143905ceedfdff88efe5757c185d7
Eunice <eakewudzie@gmail.com>
2026-07-21 21:44:09 +0200

    updated

--- freeCPractice/dataT.py
+++ freeCPractice/dataT.py
@@ -6,5 +6,6 @@ print(type(my_integerVar))
 
 
 account_balance = '12'
-
+age = 20
 print(isinstance(account_balance, int)) #this returns a boolean value to let you know the type of the variable, whether what you have stated is true or false.
+print(isinstance(age, int)) #this returns a boolean value to let you know the type of the variable, whether what you have stated is true or false.
