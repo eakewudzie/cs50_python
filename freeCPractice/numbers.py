799363632bd4d47c1622915c435086a1888b0a45
Eunice <eakewudzie@gmail.com>
2026-09-01 18:45:16 +0200

    updated

--- freeCPractice/numbers.py
+++ freeCPractice/numbers.py
@@ -1,1 +1,12 @@
-#intergers and floats 
+#integers and floats
+my_int_1 = 56
+my_int_2 = 12
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+floor_div_ints = my_int_1 // my_int_2
+floor_div_floats = my_float_2 // my_float_1
+
+print('Integer Floor Division:', floor_div_ints) # Integer Floor Division: 4
+print('Float Floor Division:', floor_div_floats) # Float Floor Division: 2.0
